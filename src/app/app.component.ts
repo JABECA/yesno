eaccdbf2912f8009d6e4cd2729fe6cecafe685ac
Alejandro <alejandromym@utp.edu.co>
2018-06-15 05:38:44 +0100

    homework3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: [ './app.component.css' ],
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: [ './app.component.css' ],
 })
-export class AppComponent {
-	// title = 'app';
-	// // url = 'https://jsonplaceholder.typicode.com/posts/1';
-	// url = 'https://yesno.wtf/api';
-	// posts$: Observable<any>;
-	// constructor(public _http: HttpClient) {
-	// 	this.posts$ = this._http
-	// 		.get(this.url)
-	// 		.pipe(filter((post) => post['id'] % 2 !== 0), pluck('answer'));
-	// 	console.log(this.posts$);
-	// }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { HideNumberPipe } from './hide-number.pipe';
+import { ExternoComponent } from './externo/externo.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ExternoComponent } from './externo/externo.component';
 
 @NgModule({
-	declarations: [ AppComponent, HideNumberPipe, HideNumberPipe,
-    ExternoComponent
-],
+	declarations: [ AppComponent, ExternoComponent ],
 	imports: [ BrowserModule, HttpClientModule ],
 	providers: [],
 	bootstrap: [ AppComponent ],
